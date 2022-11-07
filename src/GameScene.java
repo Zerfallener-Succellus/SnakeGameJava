1d43b9545c4283902981a67d6d3049b4ba4f88c9
Zerfallener-Succellus <pedrolucf@gmail.com>
2022-11-07 19:53:10 -0300

    Preparando o inicio da "Game Scene"

--- src/GameScene.java
+++ src/GameScene.java
@@ -1,6 +1,13 @@
 import java.awt.*;
 
 public class GameScene extends Scene{
+    Rect background, foreground;
+
+    public GameScene(){
+        background = new Rect(0,0,Constants.SCREEN_WIDTH,Constants.SCREEN_HEIGHT);
+        foreground = new Rect(24,48,24 * 31, 24*22);
+        /*Isso cira um grid de 31 colunas e 22 rows todos com largura e altura de 24 :D*/
+    }
 
     @Override
     public void update(double dt) {
